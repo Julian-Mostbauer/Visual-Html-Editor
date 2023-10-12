f92a2c72f7e76bcf5a6b3d440bc159698f54e0f5
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-10-12 12:57:35 +0200

    added css class for buttons

--- VHE/src/App.tsx
+++ VHE/src/App.tsx
@@ -5,11 +5,15 @@ import ToggleButton from './compenents/toggleButton'
 function App() {
 
   const [selected, setSelected] = useState(false)
-  
+  const [selected2, setSelected2] = useState(false)
+  const [selected3, setSelected3] = useState(false)
+
   return (
     <>
       <div className="card">
-      <ToggleButton func={() => setSelected(!selected)} state={selected}/>
+      <ToggleButton style="card_item" func={() => setSelected(!selected)} state={selected}/>
+      <ToggleButton style="card_item" func={() => setSelected2(!selected2)} state={selected2}/>
+      <ToggleButton style="card_item" func={() => setSelected3(!selected3)} state={selected3}/>
       </div>
     </>
   )
