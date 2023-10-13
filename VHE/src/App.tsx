ee9e5869c90086d8dea2e91da29242e689c5d392
Erik-R-1 <erik.reitbauer@outlook.de>
2023-10-13 13:37:25 +0200

    Mouse Tracker and Movable Item Test

--- VHE/src/App.tsx
+++ VHE/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState } from 'react'
 import ToggleButton from './compenents/toggleButton.tsx'
+import getMouse from './compenents/mouseTracker.tsx'
 
 function App() {
 
@@ -11,10 +12,22 @@ function App() {
   return (
     <>
       <div className="card">
-      <ToggleButton style="card_item" func={() => setSelected(!selected)} state={selected}/>
-      <ToggleButton style="card_item" func={() => setSelected2(!selected2)} state={selected2}/>
-      <ToggleButton style="card_item" func={() => setSelected3(!selected3)} state={selected3}/>
+        <ToggleButton style="card_item" func={() => setSelected(!selected)} state={selected}/>
+        <ToggleButton style="card_item" func={() => setSelected2(!selected2)} state={selected2}/>
+        <ToggleButton style="card_item" func={() => setSelected3(!selected3)} state={selected3}/>
+      </div>
+      <div>
+      <p>{getMouse().xPos}</p>
+      <p>{getMouse().yPos}</p>
+      <p>{getMouse().pressed ? "on" : "off"}</p>
+      </div>
 
+      <div className="movable_item" style = {{
+        position: "absolute",
+        left: `${getMouse().xPos}px`,
+        top: `${getMouse().yPos}px`,
+      }}>
+        <p>aaa</p>
       </div>
     </>
   )
