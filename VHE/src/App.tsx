cfd68a1d34a31b962fae347dce77ca4df778be9c
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-10-13 11:34:05 +0200

    Made the code more reusable
    Turned the many buttons in to one bouton group component

--- VHE/src/App.tsx
+++ VHE/src/App.tsx
@@ -1,20 +1,14 @@
 import './App.css'
 import { useState } from 'react'
 import ToggleButton from './compenents/toggleButton.tsx'
+import RadioCollection from './compenents/radioCollection.tsx'
 
 function App() {
-
-  const [selected, setSelected] = useState(false)
-  const [selected2, setSelected2] = useState(false)
-  const [selected3, setSelected3] = useState(false)
 
   return (
     <>
       <div className="card">
-      <ToggleButton style="card_item" func={() => setSelected(!selected)} state={selected}/>
-      <ToggleButton style="card_item" func={() => setSelected2(!selected2)} state={selected2}/>
-      <ToggleButton style="card_item" func={() => setSelected3(!selected3)} state={selected3}/>
-
+      <RadioCollection count={4} func={() => setSelected}/>
       </div>
     </>
   )
