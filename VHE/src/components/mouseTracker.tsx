5d7d0552ab17872df12aeaf3917c2693cf77c1cd
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-10-21 16:55:58 +0200

    Update mouseTracker.tsx
    -Moved the doc-block down
    -Formated

--- VHE/src/components/mouseTracker.tsx
+++ VHE/src/components/mouseTracker.tsx
@@ -1,3 +1,58 @@
+import { useEffect, useState } from "react";
+
+export default function GetMouse() {
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  const [pressed, setPressed] = useState(false);
+  const [target, setTarget] = useState(null);
+
+  const mouseUpdateHandler = (event: MouseEvent) => {
+    setMouseX(event.clientX);
+    setMouseY(event.clientY);
+  };
+
+  const mousePressHandler = () => {
+    setPressed(true);
+  };
+
+  const mouseReleaseHandler = () => {
+    setPressed(false);
+  };
+
+  const mouseTargetHandler = (event: any) => {
+    setTarget(event.target);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseUpdateHandler);
+    window.addEventListener("mousedown", mousePressHandler);
+    window.addEventListener("mouseup", mouseReleaseHandler);
+    window.addEventListener("mouseover", mouseTargetHandler);
+    return () => {
+      window.removeEventListener("mousemove", mouseUpdateHandler);
+      window.removeEventListener("mousedown", mousePressHandler);
+      window.removeEventListener("mouseup", mouseReleaseHandler);
+      window.removeEventListener("mouseover", mouseTargetHandler);
+    };
+  }, []);
+
+  return new MouseInfoSave(mouseX, mouseY, pressed, target);
+}
+
+export class MouseInfoSave {
+  xPos;
+  yPos;
+  pressed;
+  target;
+
+  constructor(xPos: number, yPos: number, pressed: boolean, target: any) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.pressed = pressed;
+    this.target = target;
+  }
+}
+
 /* --------------------------------------------Usage of MouseTracker ----------------------------------------------*/
 /*                                                                                                                 */
 /*                                                    Example                                                      */
@@ -10,60 +65,4 @@
 /*                               Always Instance the getMouse() object to avoid Errors                             */
 /*                                                                                                                 */
 /* --------------------------------------------------------------------------------------------------------------- */
-
-import { useEffect, useState } from "react";
-
-export default function GetMouse() {
-
-    const [mouseX, setMouseX] = useState(0);
-    const [mouseY, setMouseY] = useState(0);
-    const [pressed, setPressed] = useState(false);
-    const [target, setTarget] = useState(null);
-
-    const mouseUpdateHandler = (event: any) => {
-        setMouseX(event.clientX)
-        setMouseY(event.clientY)
-    }
-
-    const mousePressHandler = () => {
-        setPressed(true);
-    }
-
-    const mouseReleaseHandler = () => {
-        setPressed(false);
-    }
-
-    const mouseTargetHandler = (event: any) => {
-        setTarget(event.target);
-    }
-
-    useEffect(()=>{
-        window.addEventListener('mousemove', mouseUpdateHandler);
-        window.addEventListener('mousedown', mousePressHandler);
-        window.addEventListener('mouseup', mouseReleaseHandler);
-        window.addEventListener('mouseover', mouseTargetHandler);
-        return(()=>{
-            window.removeEventListener('mousemove', mouseUpdateHandler);
-            window.removeEventListener('mousedown', mousePressHandler);
-            window.removeEventListener('mouseup', mouseReleaseHandler);
-            window.removeEventListener('mouseover', mouseTargetHandler);
-        })
-    }, []) 
-
-    return new MouseInfoSave(mouseX, mouseY, pressed, target)
-}
-
-export class MouseInfoSave {
-    xPos;
-    yPos;
-    pressed;
-    target;
-
-    constructor(xPos: number, yPos: number, pressed: boolean, target: any) {
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.pressed = pressed;
-        this.target = target;
-    }
-}
 
