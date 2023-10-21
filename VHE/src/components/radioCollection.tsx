22ff4d2953fc5ab9f77138834768f5ef1d35f768
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-10-21 16:57:54 +0200

    More formating

--- VHE/src/components/radioCollection.tsx
+++ VHE/src/components/radioCollection.tsx
@@ -1,3 +1,66 @@
+import ToggleButton from "./toggleButton";
+import { useState } from "react";
+
+interface Prop {
+  count: number;
+  tile_mode: string;
+}
+
+function setStateAtIndex(
+  currentStates: any,
+  index: number,
+  set_fn: (a: any) => void
+) {
+  let newStates = [...currentStates]; // create a new copy of the array
+  newStates[index] = !newStates[index]; // update the new copy
+  set_fn(newStates); // set the new state
+}
+
+export default function RadioCollection(props: Prop) {
+  const monkeys = []; //funny
+  const initBools = [];
+  for (let i = 0; i < props.count; i++) {
+    monkeys.push(i);
+    initBools.push(false);
+  }
+  const [currentStates, setStates] = useState(initBools);
+
+  if (props.tile_mode == "right") {
+    return (
+      <table>
+        {monkeys.map((index) => (
+          <td>
+            <ToggleButton
+              func={() => setStateAtIndex(currentStates, index, setStates)}
+              state={currentStates[index]}
+              style="card"
+            />
+          </td>
+        ))}
+      </table>
+    );
+  } else if (props.tile_mode == "down") {
+    return (
+      <table>
+        {monkeys.map((index) => (
+          <ul>
+            <ToggleButton
+              func={() => setStateAtIndex(currentStates, index, setStates)}
+              state={currentStates[index]}
+              style="card"
+            />
+          </ul>
+        ))}
+      </table>
+    );
+  } else {
+    return (
+      <>
+        <h1>You fucked up</h1>
+      </>
+    );
+  }
+}
 /* -------------------------------------------Usage of RadioCollection --------------------------------------------*/
 /*                                                                                                                 */
 /*                                                   Required                                                      */
@@ -15,65 +78,3 @@
 /*                                  tile_mode has "right" or "down" and nothing else                               */
 /*                                                                                                                 */
 /* --------------------------------------------------------------------------------------------------------------- */
-
-import ToggleButton from "./toggleButton";
-import { useState } from 'react'
-
-interface Prop {
-    count: number;
-    tile_mode: string;
-}
-
-function setStateAtIndex(currentStates:any, index:number, set_fn: (a: any) => void){
-    let newStates = [...currentStates]; // create a new copy of the array
-    newStates[index] = !newStates[index]; // update the new copy
-    set_fn(newStates); // set the new state
-}
-
-
-export default function RadioCollection(props:Prop){
-    const monkeys = []; //funny
-    const initBools = [];
-    for(let i = 0; i < props.count; i++){
-        monkeys.push(i)
-        initBools.push(false);
-    }
-    const [currentStates, setStates] = useState(initBools);
-
-
-    if(props.tile_mode == "right"){
-        return (
-          <table>
-            {monkeys.map(index => (
-                <td>
-                    <ToggleButton 
-                    func={() => setStateAtIndex(currentStates, index, setStates)} 
-                    state={currentStates[index]} 
-                    style="card"/>
-                </td>
-                ))}
-                </table>
-        );
-    }else if(props.tile_mode == "down"){
-        return (
-            <table>
-              {monkeys.map(index => (
-                  <ul>
-                      <ToggleButton 
-                      func={() => setStateAtIndex(currentStates, index, setStates)} 
-                      state={currentStates[index]} 
-                      style="card"/>
-                  </ul>
-                  ))}
-                  </table>
-          );
-    }else{
-        return(
-            <>
-                <h1>You fucked up</h1>
-            </>
-        )
-    }
-
-
-}
